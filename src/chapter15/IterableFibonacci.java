828236c618f1c8146704923af6580feb447cde5e
xiuling216 <xiuling216@163.com>
2013-12-05 09:44:30 +0800

    chapter15

---
+++ src/chapter15/IterableFibonacci.java
@@ -0,0 +1,37 @@
+package chapter15;
+
+import java.util.Iterator;
+
+/**
+ * @echo 2013-12-4
+ * 
+ */
+public class IterableFibonacci extends Fibonacci implements Iterable<Integer> {
+	private int n;
+
+	public IterableFibonacci(int count) {
+		n = count;
+	}
+
+	public Iterator<Integer> iterator() {
+		return new Iterator<Integer>() {
+			public boolean hasNext() {
+				return n > 0;
+			}
+
+			public Integer next() {
+				n--;
+				return IterableFibonacci.this.next();
+			}
+
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+	
+	public static void main(String[] args){
+		for(int i: new IterableFibonacci(18))
+			System.out.print(i+" ");
+	}
+}
